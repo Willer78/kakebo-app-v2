3bb93425c98b97fc2d01bcfb63ccc8bd339e5655
Willer78 <scovino.william@gmail.com>
2025-08-31 13:15:09 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,3 @@
-
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { console.log('Service Worker attivato'); });
 self.addEventListener('fetch', e => {});
